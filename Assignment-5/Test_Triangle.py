42804faa82c41000b08470703a8f6b7259f8a19a
dhriti24 <shah.dhriti@gmail.com>
2023-10-15 19:07:09 -0400

    After Pylint and Before Coverage

--- Assignment-5/Test_Triangle.py
+++ Assignment-5/Test_Triangle.py
@@ -4,34 +4,40 @@ Author : Dhriti Shah
 Assignment : Assignment 2a
 
 """
-from Triangle import classifyTriangle
+from check_triangle import classify_triangle
 
 def test_invalid_input():
-    assert classifyTriangle(2000, 3000, 4000) == 'InvalidInput'
-    assert classifyTriangle(1, 1.1, 1) == 'InvalidInput'
-    assert classifyTriangle(-1, 1, 1) == 'InvalidInput'
+    ''' This function tests the invalid inputs'''
+    assert classify_triangle(2000, 3000, 4000) == 'InvalidInput'
+    assert classify_triangle(1, 1.1, 1) == 'InvalidInput'
+    assert classify_triangle(-1, 1, 1) == 'InvalidInput'
 
 def test_not_a_triangle():
-    assert classifyTriangle(1, 1, 3) == 'NotATriangle'
-    assert classifyTriangle(6, 6, 14) == 'NotATriangle'
-    assert classifyTriangle(3, 4, 9) == 'NotATriangle'
+    ''' This function tests the NotATriangle'''
+    assert classify_triangle(1, 1, 3) == 'NotATriangle'
+    assert classify_triangle(6, 6, 14) == 'NotATriangle'
+    assert classify_triangle(3, 4, 9) == 'NotATriangle'
 
 def test_equilateral():
-    assert classifyTriangle(1, 1, 1) == 'Equilateral'
-    assert classifyTriangle(100, 100, 100) == 'Equilateral'
-    assert classifyTriangle(200, 100, 200) != 'Equilateral'
+    ''' This function tests the Equilateral'''
+    assert classify_triangle(1, 1, 1) == 'Equilateral'
+    assert classify_triangle(100, 100, 100) == 'Equilateral'
+    assert classify_triangle(200, 100, 200) != 'Equilateral'
 
 def test_right():
-    assert classifyTriangle(3, 4, 5) == 'Right'
-    assert classifyTriangle(8, 6, 10) == 'Right'
-    assert classifyTriangle(4, 5, 6) != 'Right'
+    ''' This function tests the Right'''
+    assert classify_triangle(3, 4, 5) == 'Right'
+    assert classify_triangle(8, 6, 10) == 'Right'
+    assert classify_triangle(4, 5, 6) != 'Right'
 
 def test_isoceles():
-    assert classifyTriangle(2, 1, 2) == 'Isoceles'
-    assert classifyTriangle(3, 3, 5) == 'Isoceles'
-    assert classifyTriangle(2, 3, 1) != 'Isoceles'
+    ''' This function tests the Isoceles'''
+    assert classify_triangle(2, 1, 2) == 'Isoceles'
+    assert classify_triangle(3, 3, 5) == 'Isoceles'
+    assert classify_triangle(2, 3, 1) != 'Isoceles'
 
 def test_scalene():
-    assert classifyTriangle(4, 2, 3) == 'Scalene'
-    assert classifyTriangle(6, 8, 12) == 'Scalene'
-    assert classifyTriangle(3, 2, 3) != 'Scalene'
+    ''' This function tests the Scalene'''
+    assert classify_triangle(4, 2, 3) == 'Scalene'
+    assert classify_triangle(6, 8, 12) == 'Scalene'
+    assert classify_triangle(3, 2, 3) != 'Scalene'
--- Assignment-5/check_triangle.py
+++ Assignment-5/check_triangle.py
@@ -6,60 +6,61 @@ Assignment : Assignment 1
 """
 
 def classify_triangle(a, b, c):
-    #ToDO
-    if(isInvalid(a, b, c)):
-        return "Invalid"
-    elif(not(isTriangle(a, b, c))):
-        return "Not a Triangle"
-    elif(isEquilateral(a, b, c)):
-        return "Equilateral"
-    elif(isIsosceles(a, b, c)):
-        return "Isosceles"
-    elif(isRight(a, b, c)):
-        return "Right"
-    else:
-        return "Scalene"
 
-def isInvalid(a, b, c):
+    ''' This function returns a string with the type of triangle from three  values'''
+    if is_invalid(a, b, c):
+        return "Invalid"
+    if not is_triangle(a, b, c):
+        return "Not a Triangle"
+    if is_equilateral(a, b, c):
+        return "Equilateral"
+    if is_isosceles(a, b, c):
+        return "Isosceles"
+    if is_right(a, b, c):
+        return "Right"
+    return "Scalene"
+
+def is_invalid(a, b, c):
+
+    ''' This function returns True if the triangle is invalid'''
     if(a <= 0 or b <= 0 or c <= 0):
         return True
     if(a > 200 or b > 200 or c > 200):
         return True
-    if(not(isinstance(a, int)) or not(isinstance(b, int)) or not(isinstance(c, int))):
+    if not isinstance(a, int) or not isinstance(b, int) or not isinstance(c, int):
         return True
-    else:
-        return False
-    
-def isTriangle(a, b, c):
+    return False
+
+def is_triangle(a, b, c):
+    ''' This function returns True if the triangle is valid'''
     if(a + b <= c or a + c <= b or b + c <= a):
         return False
-    else:
-        return True
-    
-def isEquilateral(a, b, c):
+    return True
+
+def is_equilateral(a, b, c):
+    ''' This function returns True if the triangle is equilateral'''
     if(a == b and b == c):
         return True
-    else:
-        return False
-    
-def isIsosceles(a, b, c):
+    return False
+
+def is_isosceles(a, b, c):
+    ''' This function returns True if the triangle is isosceles'''
     if(a == b or a == c or b == c):
         return True
-    else:
-        return False
-    
-def isRight(a, b, c):
+    return False
+
+def is_right(a, b, c):
+    ''' This function returns True if the triangle is right'''
     if(a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2):
         return True
-    else:
-        return False
-    
-def isScalene(a, b, c):
+    return False
+
+def is_scalene(a, b, c):
+    ''' This function returns True if the triangle is scalene'''
     if(a != b and a != c and b != c):
         return True
-    else:
-        return False
-    
+    return False
+
 if __name__ == '__main__':
     print('classify_triangle(3, 3, 2) = ' + classify_triangle(3, 3, 2))
     print('classify_triangle(3, 3, 3) = ' + classify_triangle(3, 3, 3))
